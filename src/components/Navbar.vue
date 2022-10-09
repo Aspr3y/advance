63621cdd01a97eae4edba5da73114c04166713c2
Armani Brown <61952310+ChromeMatic@users.noreply.github.com>
2022-10-08 20:26:39 -0500

    Added navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="uk-navbar-container" uk-navbar uk-sticky>
+    <div class="uk-navbar-left">
+      <ul class="uk-navbar-nav">
+        <li class="uk-active">home<a href=""></a></li>
+        <li class="uk-parent">services<a href=""></a></li>
+        <li><a href=""></a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
