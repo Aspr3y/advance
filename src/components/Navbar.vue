15ee208637b220559495111780238d2ce9464160
Armani Brown <61952310+ChromeMatic@users.noreply.github.com>
2022-10-08 20:51:00 -0500

    Additional changes added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,11 @@
 <template>
   <nav class="uk-navbar-container" uk-navbar uk-sticky>
-    <div class="uk-navbar-left">
-      <ul class="uk-navbar-nav">
-        <li class="uk-active">home<a href=""></a></li>
-        <li class="uk-parent">services<a href=""></a></li>
-        <li><a href=""></a></li>
+    <div class="uk-navbar-center">
+      <ul class="uk-navbar-nav navBg">
+        <li class="uk-active"><router-link to="/">Home</router-link></li>
+        <li class="uk-active"><router-link to="/Services">Services</router-link></li>
+        <li class="uk-active"><router-link to="/Booking">Booking</router-link></li>
+        <li class="uk-active"><router-link to="/About">About</router-link></li>
       </ul>
     </div>
   </nav>
@@ -17,5 +18,6 @@ export default {
 </script>
 
 <style scoped>
-
+   nav{background-color:lightblue;}
+  .navBg li{ font-size: 18px;}
 </style>
--- src/components/menuBar.vue
+++ src/components/menuBar.vue
@@ -1,39 +1,9 @@
 <template>
 <div>
   <div class="uk-position-relative navy">
+
     <img src="../assets/head_img.jpg" width="1800" height="1200" alt="">
-    <div class="uk-position-top">
-      <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
-        <div class="uk-navbar-right">
-          <ul class="uk-navbar-nav">
-            <li class="uk-active"><router-link to="/">Home</router-link></li>
-            <li><router-link to="/Services">Services</router-link></li>
-            <li><router-link to="/Booking">Booking</router-link></li>
-            <li><router-link to="/About">About</router-link></li>
-          </ul>
-          <div>
-          <a href="#" class="uk-icon-link uk-margin-small-left" uk-icon="user"></a>
-          </div>
-          </div>
-      </nav>
 
-      <div class="uk-section">
-        <div class="uk-container">
-
-          <h3></h3>
-
-          <div class="uk-grid-match uk-child-width-1-2@m" uk-grid>
-            <div>
-              <p>Quality</p>
-              <p>Landscaping </p>
-              <p> Reliable</p>
-              <p>Services</p>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </div>
   </div>
 </div>
 </template>
@@ -45,8 +15,5 @@ export default {
 </script>
 
 <style scoped>
-.navy{
 
-
-}
 </style>
